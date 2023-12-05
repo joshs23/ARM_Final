7ca3d7bf58f1e749998ac2085fc48de5d1b988d1
WilliamKluck <92551689+WilliamKluck@users.noreply.github.com>
2023-12-05 02:34:49 -0800

    _ralloc changed
    in this commit, I walked through heap.c and mimicked _ralloc line by line

--- heap.s
+++ heap.s
@@ -18,7 +18,7 @@ INVALID		EQU		-1				; an invalid id
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Memory Control Block Initialization
 		EXPORT	_heap_init
-_heap_init
+_heap_init ;FINISHED
 	;; Implement by yourself
 		;Initialize the first entry (mcb[0]) to indicate the entire 16KB space is available
 		LDR		r1, =MCB_TOP
@@ -26,13 +26,13 @@ _heap_init
 		STRH	r0, [r1]
 		
 		;Zero-initialize the rest of the entries
-		LDR		r1, =MCB_TOP + 2 ; starting at the 2nd entry (mcb[1])
+		LDR		r1, =MCB_TOP + 0x2 ; starting at the 2nd entry (mcb[1])
 		MOV		r0, #0x0
-		MOV		r2, #MCB_TOTAL - 1 ; Number of Entries to initialize
+		MOV		r2, #MCB_TOTAL - 0x1 ; Number of Entries to initialize
 		
 _heap_init_loop
-		STRH	r0, [r1], #2 ; Zero-initialize the entry
-		SUBS	r2, r2, #1
+		STRH	r0, [r1], #0x2 ; Zero-initialize the entry
+		SUBS	r2, r2, #0x1
 		BNE		_heap_init_loop
 		
 		; return
@@ -47,45 +47,87 @@ _ralloc ; FUNCTION IS NOT FINISHED +++++++++++++++++++++++++++++++++++++++++++++
 		; r0 = size
 		; r1 = start address of the current range (left or right)
 		; r2 = end address of the current range
-		; r3 = current MCB index
-		
-		PUSH	{lr}
-		; Calculate the size of the current range
-		SUB		r4, r2, r1
-		
-		; Check if the current range is available and fits the requested size
-		LDR		r5, [r3] ; Load the MCB entry
-		CMP		r5, #0 ; Check if the range is available
-		BNE		_ralloc_skip_allocation
-		
-		CMP		r4, r0 ; Check if the range size is greater than or equal to the requested size
-		BLT		_ralloc_skip_allocation
-		
-		; Allocate the current range
-		STR		r4, [r3] ; Record the size of the allocated range in the MCB
+		; r3 = mcb_ent_sz
 
-		; Calculate the address of the right half
-		ADD		r6, r1, r4
+		PUSH	{lr}
+		SUB		r4, r2, r1 
+		ADD		r4, r4, r3 ; mcb_entire_size = right - left + mcb_ent_sz
 		
-		; Update the MCB entry for the right half
-		STR		r4, [r6]
+		ASR		r5, r4, #0x1 ; mcb_half_size = mcb_entire_size / 2
 		
-		MOV		r0, r1 ; Return the start address of the allocated range
-		POP		{lr}
-		MOV		pc, lr ; Return from _ralloc
+		ADD		r6, r1, r5 ; midpoint = left + mcb_half_size
+		MOV 	r12, #0x0	; heap_adr = NULL
+		LSL		r8, r4, #0x4 ; act_entire_size = mcb_entire_size * 16
+		LSL		r9, r5, #0x4; act_half_size = mcb_half_size * 16
 		
-_ralloc_skip_allocation
-		; Check if the current range can be further divided
-		CMP		r4, r0 ; Check if the current range size is ess than the requested size
-		BLT		_ralloc_no_allocation
+		CMP		r0, r9 ; if(size <= act_half_size)
+		BGT		_else
 		
-		;		Recursively call _ralloc for the left half
+		PUSH 	{r0-r9}
+		SUB		r2, r6, r3 ; right = midpoint - mcb_ent_sz
 		BL		_ralloc
-		MOV		pc, lr ; return from _ralloc
+		POP		{r0-r9}
 		
-_ralloc_no_allocation
-		MOV		r0, #0 ; return 0 to indicate failure
-		MOV		pc, lr ; return from _ralloc
+		CMP		r12, #0x0
+		BEQ		_rightalloc
+		
+		; if ((array[m2a(midpoint)] & 0x01) == 0
+		LDR		r10, [r6] ; value = value at the midpoint
+		AND		r10, r10, #0x1
+		CMP		r10, #0x0
+		BEQ		_found
+		
+		B		done
+		
+_rightalloc
+		; _ralloc(size, midpoint, right)
+		PUSH	{r0-r9}
+		MOV 	r1, r6 ; start address of current range = midpoint
+		BL		_ralloc
+		POP		{r0-r9}
+		BL 		done
+		
+		
+_else
+		LDR		r10, [r1] ; value = value at start address of current range
+		AND		r10, r10, #0x1 ; value = either 0 or 1
+		CMP		r10, #0x0 ; if(value == 0)
+		BNE		_return_invalid ; i.e. used
+		
+		LDR		r10, [r1] ; value = value at start address of current range
+		CMP		r10, r8 ; if(value < act_entire_size
+		BLT		_return_invalid ; i.e. can't fit
+		
+		ORR		r10, r8, #0x1
+		STR		r10, [r1]
+		
+		; return (void *)( heap_top + ( left - mcb_top ) * 16)
+		LDR		r10, =MCB_TOP
+		LDR		r11, =HEAP_TOP
+		
+		SUB		r1, r1, r10 ; (left - mcb_top)
+		LSL		r1, r1, #0x4 ; (left - mcb_top) * 16
+		ADD		r11, r11, r1
+		MOV		r12, r11
+		
+		B 		done
+		
+		
+_found
+		STR		r9, [r6] 
+		B		done
+		
+_return_invalid
+		MOV		r12, #0 ; heap_addr = still not found (NULL)
+		B		done
+		
+done
+		POP		{lr}
+		BX		lr
+		
+		
+		
+		
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Kernel Memory Allocation
 ; void* _k_alloc( int size )
@@ -94,14 +136,14 @@ _kalloc ; FUNCTION IS NOT FINISHED
 	;; Implement by yourself
 		; r0 = size
 		; Save return location
-		PUSH	{LR}
+		PUSH	{lr}
 		; Call the recursive allocation function
 		LDR		r1, =MCB_TOP ; start address of the entire range
 		LDR		r2, =MCB_BOT ; end address of the entire range
 		LDR		r3, =MCB_ENT_SZ ; size of each MCB entry (2 bytes)
 		BL		_ralloc ; branch to _ralloc (recursive allocation)
 		
-		POP		{LR}
+		POP		{lr}
 		MOV		pc, lr ; return to _sys_malloc (svs.s)
 		
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
